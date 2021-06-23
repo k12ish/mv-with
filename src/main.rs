aebc6f2a7b9a9b9f57213ad94783a3f5dbc4271d
k12ish <k4krish@gmail.com>
2021-06-23 22:39:15 +0100

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use clap::App;
+use clap::Arg;
+
+fn main() {
+    let matches = App::new("rename-via")
+        .version("0.1")
+        .author("Krish S. <k4krish@gmail.com>")
+        .about("Renames files with your preferred editor")
+        .arg(
+            Arg::with_name("EDITOR")
+                .help("Sets the editor to use")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+}
