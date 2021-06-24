4cf946480ac78bcab2adc6c37f19f5bea2ef0a93
k12ish <k4krish@gmail.com>
2021-06-24 11:00:46 +0100

    Launch Editor Correctly

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use clap::App;
-use clap::Arg;
 use std::fs;
 use std::io;
 use std::io::Read;
+use std::process::Command;
+
+use atty::Stream;
+use clap::App;
+use clap::Arg;
 
 fn main() -> io::Result<()> {
     let matches = App::new("rename-via")
@@ -17,7 +20,20 @@ fn main() -> io::Result<()> {
         )
         .get_matches();
     let mut buffer = String::new();
-    io::stdin().read_to_string(&mut buffer)?;
+    if atty::is(Stream::Stdin) {
+        buffer.push_str("No StdIn :(");
+    } else {
+        io::stdin().read_to_string(&mut buffer)?;
+    }
     fs::write("/tmp/rename-via", buffer)?;
+
+    let editor = matches.value_of("EDITOR").unwrap();
+    Command::new("/usr/bin/sh")
+        .arg("-c")
+        .arg(format!("{} /tmp/rename-via", editor))
+        .spawn()
+        .expect("Error: Failed to run editor")
+        .wait()
+        .expect("Error: Editor returned a non-zero status");
     Ok(())
 }
