8b462ed36ae8851f91483ce212650ef7f113bf85
k12ish <k4krish@gmail.com>
2021-06-24 10:11:09 +0100

    Read StdIn, Write file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use clap::App;
 use clap::Arg;
+use std::fs;
+use std::io;
+use std::io::Read;
 
-fn main() {
+fn main() -> io::Result<()> {
     let matches = App::new("rename-via")
         .version("0.1")
         .author("Krish S. <k4krish@gmail.com>")
@@ -13,4 +16,8 @@ fn main() {
                 .index(1),
         )
         .get_matches();
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer)?;
+    fs::write("/tmp/rename-via", buffer)?;
+    Ok(())
 }
