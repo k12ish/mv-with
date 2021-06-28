3ceb1bf8c940c3a812839535168fc8cedea625b3
k12ish <k4krish@gmail.com>
2021-06-28 20:56:03 +0100

    FileList struct, with TryFrom<ReadDir>

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,30 @@
+use std::convert::TryFrom;
 use std::fs;
+use std::fs::ReadDir;
 use std::io;
 use std::io::Read;
+use std::path::PathBuf;
 use std::process::Command;
 
 use atty::Stream;
 use clap::App;
 use clap::Arg;
+
+struct FileList {
+    filenames: Vec<PathBuf>,
+}
+impl TryFrom<ReadDir> for FileList {
+    type Error = io::Error;
+
+    fn try_from(read_dir: ReadDir) -> Result<Self, Self::Error> {
+        let (len, _) = read_dir.size_hint();
+        let mut filenames = Vec::with_capacity(len);
+        for dir_entry in read_dir {
+            filenames.push(dir_entry?.path());
+        }
+        Ok(FileList { filenames })
+    }
+}
 
 fn main() -> io::Result<()> {
     let matches = App::new("rename-via")
@@ -19,9 +38,12 @@ fn main() -> io::Result<()> {
                 .index(1),
         )
         .get_matches();
+
     let mut buffer = String::new();
+
     if atty::is(Stream::Stdin) {
-        buffer.push_str("No StdIn :(");
+        let read_dir = fs::read_dir("./").unwrap();
+        let file_list = FileList::try_from(read_dir);
     } else {
         io::stdin().read_to_string(&mut buffer)?;
     }
